a64e94fa8bd4e5c714692fe6fa79f1d58d017dcd
Tema <93534577+SouthKartman@users.noreply.github.com>
2023-02-15 22:00:10 +0300

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFeeWP8U7y2ZlXk4BLQ1wX6wvhg4ieC3o",
+  authDomain: "worlworksocial.firebaseapp.com",
+  projectId: "worlworksocial",
+  storageBucket: "worlworksocial.appspot.com",
+  messagingSenderId: "396419686838",
+  appId: "1:396419686838:web:25ad1c9d3a2022ef0f6117"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
